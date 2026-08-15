299671ae134c36dbae94b4baad676dad69bc94df
issapsesay1 <issabsesay@gmail.com>
2026-08-15 13:35:00 -0400

    Add Sentinel configuration for Azure Log Analytics

---
+++ terraform/sentinel.tf
@@ -0,0 +1,20 @@
+hcl
+resource "azurerm_log_analytics_workspace" "trap_law" {
+  name                = "hacker-trap-workspace"
+  location            = azurerm_resource_group.trap_rg.location
+  resource_group_name = azurerm_resource_group.trap_rg.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "sentinel" {
+  solution_name         = "SecurityInsights"
+  location              = azurerm_resource_group.trap_rg.location
+  resource_group_name   = azurerm_resource_group.trap_rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.trap_law.id
+  workspace_name        = azurerm_log_analytics_workspace.trap_law.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/SecurityInsights"
+  }
+}
