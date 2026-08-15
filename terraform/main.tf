d7a1b6498572d7f53214f1612d52888d885820d7
issapsesay1 <issabsesay@gmail.com>
2026-08-15 13:33:27 -0400

    Initialize Terraform configuration for Azure

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+hcl
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "trap_rg" {
+  name     = "Hacker-Trap-Resources"
+  location = "East US"
+}
