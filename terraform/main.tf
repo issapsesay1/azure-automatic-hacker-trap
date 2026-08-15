e835313080d2c65a4f1d6df6ed175c0cd39f0fac
issapsesay1 <issabsesay@gmail.com>
2026-08-15 16:17:15 -0400

    Rename resource group to avoid duplication error

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "trap_rg" {
-  name     = "Hacker-Trap-Zone-Issa-V2"  # <-- This brand new name clears the duplicate error!
+  name     = "Hacker-Trap-Zone-Issa-V289"  # <-- Your custom V289 code clears the duplicate block!
   location = "East US"
 }
